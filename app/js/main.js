4074527e74dbefeb5da1b076a4d27036829f0e38
Luis Miguel Pérez Pérez <luismiguelperezperez@MacBook-Pro.home>
2021-02-18 21:32:40 +0100

    funcion que crea el elemento tarea

--- app/js/main.js
+++ app/js/main.js
@@ -28,7 +28,8 @@ botonEnviar.addEventListener("click", () => {
   validarString(descripcion, 500);
   validarInt(prioridad, 1, 5);
   validarFecha(fechaLimite);
-  const nuevaTarea = {
+  crearTarea(1234,nombre, fechaLimite, descripcion);
+  /*const nuevaTarea = {
     nombre,
     fechaLimite,
     descripcion,
@@ -43,7 +44,7 @@ botonEnviar.addEventListener("click", () => {
     .catch((err) => {
       console.log(err);
       alertaError(err);
-    });
+    });*/
 });
 
 
@@ -87,4 +88,71 @@ function validarInt(number, min, max) {
 
 function validarFecha(fecha) {
   return fecha instanceof Date;
+}
+
+function crearTarea(idTarea, titulo, fecha, descripcion){
+  date = new Date(fecha);
+  let day = date.getDate()
+  let month = date.getMonth() + 1
+  let year = date.getFullYear()
+
+  if(month < 10){
+    date = `${day}/0${month}/${year}`;
+  }else{
+    date = `${day}/${month}/${year}`;
+  }
+  fecha = date;
+  const main = document.querySelector("main .container");
+  let contenedorTarea = document.createElement("div");
+  contenedorTarea.id = idTarea;
+  contenedorTarea.className = "card tarea";
+
+  let cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+  contenedorTarea.appendChild(cardBody);
+
+  let row = document.createElement("div");
+  row.className = "row";
+  cardBody.appendChild(row);
+
+  let colMd6 = document.createElement("div");
+  colMd6.className = "col-md-6";
+  row.appendChild(colMd6);
+
+  let h4 = document.createElement("h4");
+  h4.innerText = titulo;
+  colMd6.appendChild(h4);
+
+  let periodo = document.createElement("span");
+  periodo.className = "beforethan";
+  periodo.innerText = "Hacer antes de "+fecha;
+  colMd6.appendChild(periodo);
+
+  let colMd6more = document.createElement("div");
+  colMd6more.className = "col-md-6 moreInfo mt-2 mt-md-0";
+  row.appendChild(colMd6more);
+
+  let contenedorBotones = document.createElement("div");
+  contenedorBotones.className="gap-2 d-grid";
+  colMd6more.appendChild(contenedorBotones);
+
+  let boton1 = document.createElement("button");
+  boton1.className="btn btn-success";
+  boton1.innerText = "Completar";
+  contenedorBotones.appendChild(boton1);
+
+  let boton2 = document.createElement("button");
+  boton2.className="btn btn-danger";
+  boton2.innerText = "Archivar";
+  contenedorBotones.appendChild(boton2);
+
+  let contenedorDescripcion = document.createElement("div");
+  contenedorDescripcion.className = "col-md-12 mt-2";
+  row.appendChild(contenedorDescripcion);
+
+  let small = document.createElement("small");
+  small.innerText = descripcion;
+  contenedorDescripcion.appendChild(small);
+
+  main.appendChild(contenedorTarea);
 }
